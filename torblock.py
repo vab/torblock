e5cadbde1760224115eb631cdcba8d0d76f4569f
V. Alex Brennen <alexbrennen@hushmail.com>
2014-01-14 23:47:41 -0500

    added ipaddr validation logic

--- torblock.py
+++ torblock.py
@@ -10,7 +10,6 @@
 #              iptables firewalling rules.
 
 import sys
-import re
 
 
 # This function will print out the usage information
@@ -24,8 +23,45 @@ def usage():
 	sys.exit(0)
 
 
+# Numeric IP address
+def numeric_ipaddr(ip):
+	quads = ip.split('.')
+	if( len(quads) != 4):
+		return False;
+	else:
+		for quad in quads:
+			byte = int(quad)
+			if( (byte <= 0) and (byte >= 255) ):
+				return False
+			else:
+				return True
+
+
+# Validate public addressable IP address (not private, loopback, or broadcast)
+def public_ipaddr(ip):
+	quads = ip.split('.')
+	# Invalid
+	if (int(quads[0]) == 0):
+		return False
+	# Loop back
+	elif (int(quads[0]) == 127):
+		return False
+	# Broadcast
+	elif((int(quads[0]) == 255) or (int(quads[1]) == 255) or (int(quads[2]) == 255) or (int(quads[3]) == 255)):
+		return False
+	# Private
+	elif(int(quads[0]) == 10):
+		return False
+	elif((int(quads[0]) == 172) and ((int(quads[1]) > 15) and (int(quads[1]) < 32))):
+		return False
+	elif((int(quads[0]) == 192) and (int(quads[1]) == 168)):
+		return False
+	else:
+		return True
+
+
 # Execute iptables command to block a node after sanity checking
-def blocknodes():
+def blocknodes(arg):
 	return
 
 
@@ -46,5 +82,5 @@ for arg in sys.argv[1:]:
 	if(arg == "-h" or arg == "--help"): 
 		usage()
 	else: 
-		blocknodes()
+		blocknodes(arg)
 
